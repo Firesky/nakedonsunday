2994eba8efa606e9a4ad5c9ca1d75fb987e36032
Luke Clifford <luke@firesky.com.au>
2023-10-23 21:56:34 +1100

    v2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         <h1>My god it's v2</h1>
         </a>
       </header>
     </div>
